16b51c3e8f780e59d38b92b580c8e4ac06f120d5
kartikey jaiswal <kartikeyjaiswal@kartikeys-MacBook-Air.local>
2026-01-21 19:12:51 +0530

    update

--- server/server.ts
+++ server/server.ts
@@ -26,7 +26,7 @@ const app = express();
 
 app.use(cors({
 
-   origin : ['http://localhost:5173', 'http://localhost:3000'],
+   origin : ['http://localhost:5173', 'http://localhost:3000' , "https://thumblify-osvu.vercel.app"],
    credentials: true
 
 
